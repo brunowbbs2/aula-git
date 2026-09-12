d04eac9d3dfba4445c936374ab384c74f92b7687
Wesley <wesley@MacBook-Pro-de-Wesley.local>
2026-09-11 21:34:34 -0300

    feat: implementa funcionalidade de clientes

---
+++ Clientes.java
@@ -0,0 +1,16 @@
+public class Clientes {
+    
+    private String nome;
+
+    public Clientes(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
