2bd9b6e00571d4a9ab360114a3f5d12a9db98bb7
Wesley <wesley@MacBook-Pro-de-Wesley.local>
2026-09-11 21:28:32 -0300

    chore: implementa construtor na classe funcionario

--- Fornecedores.java
+++ Fornecedores.java
@@ -1,3 +1,16 @@
 public class Fornecedores {
-    
+
+    private String nome;
+
+    public Fornecedores(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
